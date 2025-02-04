f3dbc2301d345e452debc49cfd34ee9f0cd0d0c5
pavankarthikparuchuri2001 <76808434+pavankarthikparuchuri2001@users.noreply.github.com>
2025-02-04 10:31:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "pavankarthik";
+
+var a = 10;
+var b = 20;
+
+console.log(name);
+console.log(a + b);
+
+console.log(global);
+console.log(this); // empty object
+console.log(globalThis); //points to the same javascript object
+console.log(globalThis === global);
